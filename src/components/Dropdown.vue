05cec414fe9f5fca5abf466bbb01a3a4ec1e62e3
Rafael Lima <rglllima@gmail.com>
2019-11-21 16:02:04 -0300

    Created Dropdown Component

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="field">
+    <div class="control">
+      <div class="select">
+        <select class="dropdown">
+          <option>{{title}}</option>
+          <option v-for="(option, index) in options" :key="index">{{option}}</option>
+        </select>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    options: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.select
+  width: 100%
+
+  select
+    background-color: #ffcc00ff
+    border: solid 1px #bdbdbd
+    width: 100%
+
+.select:not(.is-multiple):not(.is-loading)::after
+  border-color: #585858
+
+</style>
