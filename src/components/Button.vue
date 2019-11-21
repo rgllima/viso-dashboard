66cb66454ea02393423e1c949a19d7622d35503d
Rafael Lima <rglllima@gmail.com>
2019-11-21 16:02:42 -0300

    Created Some Basic Components

---
+++ src/components/Button.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="has-text-centered">
+    <button class="button" :class="color" @click="onClick">{{title}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      default: "is-primary"
+    },
+    onClick: {
+      type: Function,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+button
+  width: 140px
+  font-weight: bold
+</style>
---
+++ src/components/CustomTitle.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="custom-title">
+    <label class="subtitle has-color-secondary">{{title}}</label>
+    <div class="custom-title__bar"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.custom-title
+  width: fit-content
+  .subtitle
+    font-size: 1.85rem
+    font-weight: 700
+
+  &__bar
+    width: 100px
+    height: 5px
+    margin-bottom: 25px
+    background-color: #ffcc00
+</style>
